5f08877bb83a27ae5c510d575f78351e5b1f47f7
Owaisi123 <165272983+Owaisi123@users.noreply.github.com>
2024-09-18 18:31:42 +0500

    Add files via upload

---
+++ chapter 14-16/index.js
@@ -0,0 +1,26 @@
+// var user=prompt('Enter your name');
+// var name='owais';
+// if (user == name) {
+//     document.write(name)
+// } else {
+//     document.write("Enter write name?")
+// }
+
+// QNO:2
+
+// var fruits=[''];
+
+// Qno:3;
+
+// var num = [''];
+
+// Qno:4;
+
+// var mixed=["owais",9];
+
+// Qno:5;
+
+// var qualification=['SSC <br>', 'HSC <br>', 'BSC <br>','BS <br>', 'BCOM <br>', 'MS <br>' , 'M.Phil. <br>' , 'PHD <br>'];
+// document.write(qualification);
+
+
